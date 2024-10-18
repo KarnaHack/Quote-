74c83a900e79403cae8f72a6ddb51ac61f461f8f
Karna Hack <nabareal007@gmail.com>
2024-10-18 21:28:25 +0530

    home page is updated

---
+++ static/js/home_page.js
@@ -0,0 +1,4 @@
+// Toggle the sidebar when the hamburger is clicked
+document.getElementById('hamburger').addEventListener('click', function() {
+  document.getElementById('sidebar').classList.toggle('active');
+});
